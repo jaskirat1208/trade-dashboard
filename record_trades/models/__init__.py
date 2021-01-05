d62a9048de819cb440ffd9ded2f8e5f3b53af2e8
Jaskirat Singh <jaskiratsingh1208@gmail.com>
2021-01-05 15:42:23 +0530

    Added documentation for index view

--- record_trades/models/__init__.py
+++ record_trades/models/__init__.py
@@ -1,4 +1,1 @@
-"""
-"""
-
 from .trade import Trade
--- record_trades/views/__init__.py
+++ record_trades/views/__init__.py
@@ -1,6 +1,23 @@
+"""
+Displays the main index page for trade_record application
+"""
+
 from django.shortcuts import HttpResponse
 
 
 # Create your views here.
 def index(request):
+    """
+    Display an individual :model:`record_trades.Trade`.
+
+    **Context**
+
+    ``Trade``
+        An instance of :model:`record_trades.Trade`.
+
+    **Template:**
+
+    :template:`myapp/my_template.html`
+    """
+
     return HttpResponse("Hello, world. You're at the polls indexes.")
